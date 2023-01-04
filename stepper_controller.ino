ff2ffbe03111a902a0647b27f09224d010dc5d8b
oliver.hamilton <oliver9523@gmail.com>
2023-01-04 19:33:39 +0000

    tidy up and add trigger control

--- stepper_controller.ino
+++ stepper_controller.ino
@@ -6,88 +6,84 @@
 // #define Y_STEP_PIN 4
 // #define Y_DIR_PIN 0
 
-#define X_STEP_PIN 16
-#define X_DIR_PIN 5
+#define STEP_PIN 16
+#define DIR_PIN 5
+#define TRIGGER_PIN1 14  //todo - check pins for D1
+#define TRIGGER_PIN2 12  //todo - check pins for D1
+
 
 const byte numChars = 16;  //-1000,-1000\n
 char receivedChars[numChars];   // an array to store the received data
 
 boolean newData = false;
-bool first_run = true;
-int steps = 0;
 float max_speed = 50000;
-//TODO - when running two motors this speed is effectvly halved...
-
-int x_steps, y_steps;
-int speed;
+int steps = 0;
+int speed = 0;
+int trigger = 0;
 
 // Define the stepper motor and the pins that is connected to
-AccelStepper x_Stepper(AccelStepper::DRIVER, X_STEP_PIN, X_DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
-// AccelStepper y_Stepper(AccelStepper::DRIVER, Y_STEP_PIN, Y_DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN); // (Type of driver: with 2 pins, STEP, DIR)
 
-// MultiStepper Steppers;
-
-int x_position = 0;
-// int y_position = 0;
-
-// long positions[2];
+int position = 0;
 
 void setup()
 {
-  x_Stepper.setMaxSpeed(max_speed);
-  // y_Stepper.setMaxSpeed(max_speed);
+  stepper.setMaxSpeed(max_speed);
 
-  // Steppers.addStepper(x_Stepper);
-  // Steppers.addStepper(y_Stepper);
+  pinMode(TRIGGER_PIN1, OUTPUT);
+  digitalWrite(TRIGGER_PIN1, LOW);
 
   Serial.begin(115200);
   Serial.println("Started...");
 }
 
-void loop()
+void fire_trigger()
 {
+  digitalWrite(TRIGGER_PIN1, LOW);    //make sure it's low first
+  digitalWrite(TRIGGER_PIN1, HIGH);   //v.quick pulse
+  digitalWrite(TRIGGER_PIN1, LOW);    //return to low
+  trigger = 0;
+}
 
+void update()
+{
   if (newData) {
     newData = false;
-    
-    // positions[0] += (long)x_steps;
-    // positions[1] += (long)y_steps;
-    // Steppers.moveTo(positions);
-    // Steppers.runSpeedToPosition();
-
-    x_position += x_steps;
-    x_Stepper.moveTo((long)x_position);
-    x_Stepper.setSpeed(max_speed);    
-    // y_position += y_steps;
-    // y_Stepper.moveTo((long)y_position);
-    // y_Stepper.setSpeed(max_speed);    
-
-    // x_steps = 0;
-    // y_steps = 0;
+    position += steps;
+    stepper.moveTo((long)position);
+    stepper.setSpeed(max_speed);
   }
 
-  if (x_Stepper.distanceToGo() != 0) {
-    x_Stepper.runSpeedToPosition();
+  if (stepper.distanceToGo() != 0) {
+    stepper.runSpeedToPosition();
   }
+}
 
-  // if (y_Stepper.distanceToGo() != 0) {
-  //   y_Stepper.runSpeedToPosition();
-  // }
-
-  //recvWithEndMarker();
-  read2();
+void loop()
+{
+  update();
+  read();
 }
 
 
-void read2(){
+void read()
+{
   if (Serial.available() > 0) {
     int bytesRead = Serial.readBytesUntil('\n', receivedChars, numChars);
     newData = true;
     receivedChars[bytesRead] = '\0';
-    int result = sscanf(receivedChars, "%d,%d", &x_steps, &speed);
+    int result = sscanf(receivedChars, "%d,%d,%d", &steps, &speed, &trigger);
 
-    if (speed>0){
+    Serial.println(steps);
+
+    if (speed > 0) {
       max_speed = (float)speed;
+      Serial.println(speed);
+    }
+
+    if (trigger > 0) {
+      fire_trigger();
+      Serial.println(trigger);
     }
   }
 }
